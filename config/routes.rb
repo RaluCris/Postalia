7c505aee37c76210767d5ac138143fdcfba33738
ralu_criss <ralu_criss@yahoo.com>
2018-03-26 16:07:49 +0100

    created Vote model,controller,and form for votes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
 
   get '/home', to: 'home#home'
   root 'posts#index'
+
+  resources :posts do
+  resources :votes, only: [:create, :destroy]
+end
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319155917) do
+ActiveRecord::Schema.define(version: 20180325120506) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -57,6 +57,16 @@ ActiveRecord::Schema.define(version: 20180319155917) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.integer  "vote"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
