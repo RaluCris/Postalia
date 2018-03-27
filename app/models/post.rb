739ed97a8b6f3c59fcf0a6f1036ab8fce55d9bb5
ralu_criss <ralu_criss@yahoo.com>
2018-03-27 18:47:20 +0100

    added a serch menu

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+  where("title LIKE ? OR content LIKE ? ", "%#{search}%", "%#{search}%") 
+end
+
 end
