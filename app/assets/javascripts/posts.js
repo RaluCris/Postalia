632e776d54853a68ca6640b961ff50f3b8a8945c
RaluCris <ralu_criss@yahoo.com>
2018-06-10 11:38:23 +0100

    added some javascript

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,4 +1,23 @@
 $(document).on('turbolinks:load', function() {
 
   $('#notice').hide().slideDown(1000).delay(3000).slideUp();
+
+
+  $('.spoiler').on('click', 'button', function(event){
+    $(this).prev().show();
+    $(this).hide();
+  });
+  // Create the "Reveal Spoiler" button
+  const $button = $('<button>Reveal Spoiler</button>');
+  //Append to web page
+  $('.spoiler').append($button);
+
+  //Hide the spoiler text
+  $('.spoiler span').hide();
+
+
+  //select title to show show_content
+  $('.title').click(function(){
+    $('.show_content').show();
+  })
 });
